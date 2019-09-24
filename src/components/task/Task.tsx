705980fe7dbff4abe8761f80a7c8a01ac95de9e3
liucannn@sina.com <liucannn@sina.com>
2019-09-24 18:18:30 +0800

    feat(component): task

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -14,6 +14,7 @@ export interface TaskState {
 }
 
 export default class Task extends React.Component<TaskProps, TaskState> {
+  liRef: HTMLLIElement;
   constructor(props: TaskProps) {
     super(props);
     const isEdit = props.edit || false;
@@ -21,17 +22,58 @@ export default class Task extends React.Component<TaskProps, TaskState> {
       isEdit,
     };
   }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.onKeyDownListener);
+    window.addEventListener('click', this.onClickListener, true);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.onKeyDownListener);
+    window.removeEventListener('click', this.onClickListener);
+  }
+
+  saveLi = (el: HTMLLIElement) => {
+    this.liRef = el;
+  };
+
+  onKeyDownListener = (event: KeyboardEvent) => {
+    if (!this.state.isEdit) return;
+    if (event.keyCode === 13) {
+      this.handleHideEditor();
+    }
+  };
+
+  onClickListener = (event: MouseEvent) => {
+    if (!this.state.isEdit) return;
+    const target = event.target as HTMLElement;
+    const isLIRefOrChild = target === this.liRef || this.liRef.contains(target);
+    console.log(isLIRefOrChild, 1111);
+    if (!isLIRefOrChild) {
+      this.handleHideEditor();
+    }
+  };
+
+  handleHideEditor = () => {
+    this.setState({ isEdit: false });
+  };
+
+  handleClickTitle = () => {
+    this.setState({ isEdit: true });
+  };
+
   render() {
     const { isEdit } = this.state;
-
     return (
-      <li className="l-task">
+      <li ref={this.saveLi} className="l-task">
         {!isEdit && (
           <div className="l-task-inner">
             <Icon className="l-task-drag" type="drag" />
             <div className="l-task-content">
               <CheckBox />
-              <span className="l-task-title">测试一下</span>
+              <span className="l-task-title" onClick={this.handleClickTitle}>
+                测试一下
+              </span>
               <span className="l-task-tags" />
               <span className="l-task-todo" />
               <span className="l-task-date" />
@@ -39,7 +81,6 @@ export default class Task extends React.Component<TaskProps, TaskState> {
             <Icon className="l-task-more" type="more" />
           </div>
         )}
-
         {isEdit && <Editor />}
       </li>
     );
