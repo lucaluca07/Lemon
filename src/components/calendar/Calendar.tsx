dc604987695e1f80e2b9edc2931c179ba386a1f8
liucannn@sina.com <liucannn@sina.com>
2019-09-25 22:47:24 +0800

    component: calendar

---
+++ src/components/calendar/Calendar.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import './calendar.less';
+
+export default class Calendar extends React.Component{
+
+  render() {
+    return (
+      <div>
+        Calendar
+      </div>
+    )
+  }
+}
