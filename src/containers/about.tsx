a82159e3beaabc76fe3a1c8f774a8738fceef3b1
can.liu <can.liu02@liulishuo.com>
2020-04-03 11:50:12 +0800

    first commit

--- src/containers/about.tsx
+++ src/containers/about.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const Index: React.FC = () => {
-  return <h1>About</h1>;
+  return <h1>About1</h1>;
 };
 
 export default Index;
--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,7 +1,62 @@
 import * as React from 'react';
 
 const Index: React.FC = () => {
-  return <h1>Index</h1>;
+  const [text, setText] = React.useState('');
+  const node = React.createRef<HTMLParagraphElement>();
+  const handleInput = (e: React.FormEvent) => {
+    e.preventDefault();
+    const text = (e.target as HTMLElement).innerHTML;
+    console.log(text);
+    const selection = window.getSelection();
+    if (selection) {
+      const range = selection.getRangeAt(0);
+      const rang = document.createRange(); // 创建一个rang对象
+      const content = (e.target as HTMLElement).firstChild;
+      if (content) {
+        const { startOffset, endOffset } = range;
+        rang.setStart(content, startOffset);
+        rang.setEnd(content, 1);
+        rang.collapse(false); // 起始位置和终止位置是否相同的布尔值
+        selection.removeAllRanges(); // 移除选中区域的range对象
+        selection.addRange(rang); // 给选中区域添加range对象
+      }
+
+      console.log(range);
+      setTimeout(() => {
+        selection.addRange(range);
+      }, 60);
+    }
+
+    console.log(selection);
+    setText(text);
+    // document.execCommand('insertText', false, text || '');
+  };
+
+  return (
+    <div>
+      <h1>Index</h1>
+      <div>
+        <p
+          style={{ height: 20, border: '1px solid #eee' }}
+          dangerouslySetInnerHTML={{ __html: text }}
+          onPaste={(e) => {
+            e.preventDefault();
+            let text = e.clipboardData.getData('text/plain') || '';
+            const tasks = text?.split(/\n+/g).filter((item) => !!item);
+            if (tasks.length > 1) {
+              console.log('多行文本', tasks);
+            } else {
+              document.execCommand('insertText', false, text || '');
+              setText(text);
+            }
+          }}
+          contentEditable
+          ref={node}
+          onInput={handleInput}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Index;
